fa7492faae6c94784e83786aeb802539e3068c9b
高木 <pastel.s.telepathy@gmail.com>
2019-11-21 20:05:21 +0900

    記事のレイアウト作成。 代入準備中。

---
+++ app/src/main/java/com/example/myfirstapp/ArticleSeiho.kt
@@ -0,0 +1,13 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ArticleSeiho : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myfirstapp/view/ArticleView.kt
@@ -0,0 +1,10 @@
+package com.example.myfirstapp.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+
+class ArticleView : FrameLayout {
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?,attrs: AttributeSet?) : super(context, attrs)
+}
